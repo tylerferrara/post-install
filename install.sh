68988a8c0a624d3068834886d815e758d928161e
tylerferrara <tylerferrara123@gmail.com>
2021-02-01 21:33:13 +0000

    added more support for debian based distros

--- install.sh
+++ install.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
+
+# detect root
+cur_user="$USER"
+
+if (( EUID == 0 ))
+then
+	echo "Don't run this installer as root!"
+	exit 1
+fi
+
 echo "Installing the basics"
-$(sudo pacman -Sy vim zsh sed git curl --noconfirm)
+
+packages="mosh vim zsh sed git curl"
+if command -v pacman &> /dev/null
+then
+	sudo pacman -Syu $packages --noconfirm
+elif command -v apt &> /dev/null
+then
+	sudo apt install $packages -y
+else
+	echo "Package manager not supported!"
+	echo "exiting..."
+	exit 1
+fi
+
 
 echo "Pulling oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 
 echo "Cloning addons"
 git clone https://github.com/zdharma/fast-syntax-highlighting.git /home/$USER/.oh-my-zsh/custom/plugins/fast-syntax-highlighting
@@ -13,14 +37,12 @@ sed -i 's/plugins=(git)/plugins=(\n\tgit\n\tfast-syntax-highlighting\n\tzsh-auto
 source /home/$USER/.zshrc
 
 # vim
+
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-
-
-
-# mkdir /home/$USER/.vim
-# mkdir /home/$USER/.vim/colors
-# cp ./sonokai.vim /home/$USER/.vim/colors/sonokai.vim
-
 cp ./vimrc /home/$USER/.vimrc
+
+sudo mkdir /root/.vim
+sudo ln -s /home/$cur_user/.vimrc /root/.vimrc
+sudo ln -s /home/$cur_user/.vim/ /root/.vim/
